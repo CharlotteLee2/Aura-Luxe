72c4ae957ed95d30f6c91f6b301fcda961f4f16f
Charlotte Lee <charlotteleenyc@gmail.com>
2025-11-24 19:09:53 -0500

    all files

---
+++ Aura Luxe/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Aura Luxe
+//
+//  Created by CharlotteLee on 11/24/25.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
---
+++ Aura LuxeTests/Aura_LuxeTests.swift
@@ -0,0 +1,17 @@
+//
+//  Aura_LuxeTests.swift
+//  Aura LuxeTests
+//
+//  Created by CharlotteLee on 11/24/25.
+//
+
+import Testing
+@testable import Aura_Luxe
+
+struct Aura_LuxeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
